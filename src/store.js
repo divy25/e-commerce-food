eabd98ab582b2f3464fcff430d8ce9b4e42cce9a
Divya Prakash <divy_prakash@outlook.com>
2022-06-26 17:59:58 +0530

    added middleware

--- src/store.js
+++ src/store.js
@@ -1,11 +1,16 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware,compose} from "redux";
 import rootred from "./redux/reducers/main";
-
-const store = createStore (
-    rootred 
-)
-    
+import thunk from 'redux-thunk';
 
 
+let composeEnhancers = compose;
 
-export default store 
+if (process.env.NODE_ENV !== 'production') {
+	composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+		? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+		: compose;
+}
+
+const enhancer = composeEnhancers(applyMiddleware(thunk));
+
+export const store = createStore(rootred, enhancer);
